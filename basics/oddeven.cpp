5904e4c36a7d72fe4105ebf808813cf2e69cf2e6
ayush6879 <97286504+ayush6879@users.noreply.github.com>
2024-02-29 22:43:19 +0530

    Create oddeven.cpp

---
+++ basics/oddeven.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    int i = 1;
+    while(i<=N){
+        if(i%2!=0){
+        cout<<i<<" ";}
+        i+=2;
+        }
+return 0;
+}
