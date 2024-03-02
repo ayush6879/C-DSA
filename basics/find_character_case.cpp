618b63dd90b597aac08c5b076bc226ae44fb1e77
ayush6879 <97286504+ayush6879@users.noreply.github.com>
2024-03-02 22:30:14 +0530

    Create find_character_case.cpp

---
+++ basics/find_character_case.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char a;
+    cin>>a;
+    if(a>=65 && a<=95){
+        cout<<"1";}
+        else if(97<=a && a<=122){
+            cout<<"0";
+        }
+        else{
+            cout<<"-1";
+        }
+
+    }
